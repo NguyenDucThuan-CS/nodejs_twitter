fa5edb96b5c1c7a87e3a0c268ac023bd52c90247
Thuan Nguyen <thuan.nguuyen@yoot.vn>
2023-10-19 22:34:17 +0700

    udpate

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -6,6 +6,24 @@ import { registerValidator } from '~/middlewares/users.milddlewares'
 const usersRouter = express.Router()
 
 usersRouter.post('/login', loginValidator, loginController)
-usersRouter.post('/register', registerValidator, registerController)
+usersRouter.post(
+  '/register',
+  registerValidator,
+  (req, res, next) => {
+    console.log('request handler 1')
+    next(new Error('cmn'))
+  },
+  (req, res, next) => {
+    next(new Error('Loi roi'))
+  },
+  (req, res, next) => {
+    console.log('123')
+    res.json({ message: 'register success' })
+  },
+  (err, req, res, next) => {
+    console.log('Loi la', err.message)
+    res.status(400).json({ error: err.message })
+  }
+)
 
 export default usersRouter
